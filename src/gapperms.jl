b8a21431580c944594fb827384e35378cf1c7528
kalmarek <kalmar@amu.edu.pl>
2019-10-25 12:17:25 +0200

    parametrize GAPPerm by UInt16, UInt32

--- src/gapperms.jl
+++ src/gapperms.jl
@@ -1,58 +1,78 @@
-function gap_header(size::Int64, flags::UInt8=0x00, type::UInt8=0x08)
-    @assert 0 < size < 2^48
+function gap_header(size::T, flags::UInt8=0x00, type::UInt8=(sizeof(T) == 4 ? 0x08 : 0x07)) where T<:Union{UInt16, UInt32}
+    # size must be less than typemax(UInt32)
     header = UInt64(0)
     header += size
-    header = header << 16
+    header = header << (sizeof(T) == 4 ? 0 : 16) # alignment to 32bit boundary
+    header = header << 8
     header += flags
     header = header << 8
     header += type
     return header
 end
 
-struct GAPPerm
-    data::Vector{UInt32}
+gaph_s(::Type{T}) where T = sizeof(UInt64) ÷ sizeof(T) # 2 or 4
+ptr_s(::Type{T}) where T = sizeof(Ptr) ÷ sizeof(T) # 2 or 4
+data_offset(::Type{GAPPerm{T}}) where T = gaph_s(T) + ptr_s(T)
 
-    function GAPPerm(d::AbstractVector{<:Integer})
-        n = length(d)
-        data = Vector{UInt32}(undef, n+4)
-        data[1:2] = reinterpret(UInt32, [gap_header(n)])
-        data[3:4] = reinterpret(UInt32, [C_NULL]) # void pointer
-        data[5:end] .= convert.(UInt32, d)
-        return new(data)
+struct GAPPerm{T<:Union{UInt16, UInt32}}
+    data::Vector{T}
+
+    function GAPPerm{T}(n::Integer) where T<:Union{UInt16, UInt32}
+        @assert 0 < n <= typemax(T)
+        data = Vector{T}(undef, n + gaph_s(T) + ptr_s(T))
+        data[1 : gaph_s(T)] = reinterpret(T, [gap_header(T(n))])
+        data[1+gaph_s(T) : gaph_s(T)+ptr_s(T)] = reinterpret(T, [C_NULL]) # void pointer
+        data[1+data_offset(GAPPerm{T}):end] .= T(1):T(n)
+
+        return new{T}(data)
     end
 
-    function GAPPerm(n::Int)
-        data = Vector{UInt64}(undef, n+4)
-        data[1:2] = reinterpret(UInt32, [gap_header(n)])
-        data[3:4] = reinterpret(UInt32, [C_NULL]) # void pointer
-        data[5:end] .= UInt32(1):UInt32(n)
+    function GAPPerm{T}(d::AbstractVector{<:Integer}) where T<:Union{UInt16, UInt32}
+        n = length(d)
+
+        @assert 0 < n <= typemax(T)
+        data = Vector{T}(undef, n + gaph_s(T) + ptr_s(T))
+        data[1 : gaph_s(T)] = reinterpret(T, [gap_header(T(n))])
+        data[1+gaph_s(T) : gaph_s(T)+ptr_s(T)] = reinterpret(T, [C_NULL]) # void pointer
+
+        data[1+data_offset(GAPPerm{T}):end] .= d
+
         return new(data)
     end
 end
 
 function Base.getindex(p::GAPPerm, n::Integer)
+function Base.getindex(p::T, n::Integer) where T<:GAPPerm
     @boundscheck 0 < n
-    return (n > degree(p) ? Int(n) : Int(p.data[n+4]))
+    return (n > degree(p) ? Int(n) : Int(p.data[n+data_offset(T)]))
 end
 
-function Base.setindex!(p::GAPPerm, v::Integer, n::Integer)
+function Base.setindex!(p::T, v::Integer, n::Integer) where T<:GAPPerm
     @boundscheck 0 < n <= degree(p)
-    return p.data[n+4] = v
+    return p.data[n+data_offset(T)] = v
 end
 
-gap_header(p::GAPPerm) = reinterpret(UInt64, p.data[1:2])[]
+function gap_header(p::T) where T<:GAPPerm
+    k = data_offset(T) ÷ 2
+    reinterpret(UInt64, p.data[1: k])[]
+end
+
 gap_flags(p::GAPPerm) = reinterpret(UInt8, p.data[1:1])[2]
 gap_type(p::GAPPerm)  = reinterpret(UInt8, p.data[1:1])[1]
-Generic.degree(p::GAPPerm) = Int(gap_header(p) >> 24)
+
+function Generic.degree(p::GAPPerm{T}) where T
+    shift = sizeof(T) == 4 ? 16 : 32
+    return Int(gap_header(p) >> shift)
+end
 
 ### Generic stuff below
 
 Base.iterate(p::GAPPerm, s=1) = (s > degree(p) ? nothing : (p[s], s+1))
-Base.eltype(::Type{GAPPerm}) = Int64
+Base.eltype(::Type{<:GAPPerm}) = Int64
 Base.length(p::GAPPerm) = degree(p)
 Base.size(p::GAPPerm) = (degree(p),)
 
-Base.similar(p::GAPPerm) = GAPPerm(degree(p))
+Base.similar(p::GAPPerm{T}) where T = GAPPerm{T}(degree(p))
 
 Base.firstindex(p::GAPPerm) = 1
 Base.lastindex(p::GAPPerm) = degree(p)
