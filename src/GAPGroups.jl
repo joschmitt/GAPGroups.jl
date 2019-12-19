23aafcadf1315ad8c4f4a3facbfce6aaa1a99385
GDeFranceschi <52538321+GDeFranceschi@users.noreply.github.com>
2019-12-19 14:19:41 +0100

    Add files via upload

--- src/GAPGroups.jl
+++ src/GAPGroups.jl
@@ -1,5 +1,42 @@
-module GAPGroups
+module PermModule
 
-greet() = print("Hello World!")
+using GAP
+export symmetric_group, MyRandom
 
-end # module
+Tg=Main.ForeignGAP.MPtr
+
+struct GAPGroup
+   X::Tg
+end
+
+struct GAPGroupElem
+   x::Tg
+end
+
+function MyOrder(x::GAPGroup)
+   return GAP.Globals.Size(x.X)
+end
+
+function MyRandom(x::GAPGroup)
+   s=GAP.Globals.Random(x.X)
+   y=GAPGroupElem(s)
+   return y
+end
+
+end
+
+function symmetric_group(n::Int64)
+   if n<1
+     throw(ArgumentError("it must be a positive integer"))
+   else
+      return PermModule.GAPGroup(GAP.Globals.SymmetricGroup(n))
+   end
+end
+
+function order(x::PermModule.GAPGroup)
+   return PermModule.MyOrder(x)
+end
+
+function random(x::PermModule.GAPGroup)
+   return PermModule.MyRandom(x)
+end
