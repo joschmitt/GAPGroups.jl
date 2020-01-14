8ef76127d476e486f03e83cc109b75de26930bdb
Giovanni <Francesco@somewhere.de>
2020-01-14 17:03:14 +0100

    some changes

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,18 +19,28 @@ end
    G=symmetric_group(i)
    x=rand(G)
    y=rand(G)
+   z=perm([j for j=1:i])
+   w=perm([1,2],[j for j=3:i])
    ox=order(x)
    oy=order(y)
-   
+   oz=order(z)
+
    @test x isa GGE
    @test ox isa Int64
    @test (x*y).X == x.X*y.X
    @test x^2 == x*x
    @test x*one(G)==x
+   @test oz==i
    @test x*x^-1 == one(G)
    @test x^(ox-1)==x^-1
-   @test x^y == y^-1*x*y
+   @test x^w == w^-1*x*w
+   @test z^y == y^-1*z*y
+   @test z(1)==2
+   @test (w^-1)(i)==i-1
+   @test x(y(1))==(y*x)(1)
    @test x>y || x==y || x<y
+   @test isequal(x,y) || isless(x,y) || isless(y,x)
+   @test (isless(x,y) && x<y) || (isequal(x,y) && x==y) || (isless(y,x) && x>y)
 end
 
 end
