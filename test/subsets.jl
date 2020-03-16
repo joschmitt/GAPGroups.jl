3b09b91f01bbc431d4c99d40ecd7ba7bc602d846
Giovanni <Francesco@somewhere.de>
2020-03-16 17:28:33 +0100

    added coset tests

--- test/subsets.jl
+++ test/subsets.jl
@@ -2,17 +2,38 @@
 
 @testset "Cosets" begin
   
-  G = dihedral_group(8)
-  H, mH = center(G)
+   G = dihedral_group(8)
+   H, mH = center(G)
   
-  @test index(G, H) == 4
+   @test index(G, H) == 4
   
-  C = right_coset(H, G[1])
-  @test order(C) == length(elements(C))
+   C = right_coset(H, G[1])
+   @test order(C) == length(elements(C))
   
-  @test length(right_cosets(G, H)) == index(G, H)
+   @test length(right_cosets(G, H)) == index(G, H)
   
-  @test length(right_transversal(G, H)) == index(G, H)
+   @test length(right_transversal(G, H)) == index(G, H)
+
+   @testset "set comparation for cosets in PermGroup" begin
+      G=symmetric_group(5)
+      x = G(cperm([1,2,3]))
+      y = G(cperm([1,4,5]))
+      z = G(cperm([1,2],[3,4]))
+      H = sub(G,[y])[1]
+      K = sub(G,[z])[1]
+
+      @test_throws ArgumentError rc = right_coset(H,K(z))
+      @test_throws ArgumentError rc = left_coset(H,K(z))
+      rc = right_coset(H,x)
+      lc = left_coset(H,x)
+      dc = double_coset(H,x,K)
+      @test acting_domain(rc) == H
+      @test representative(rc) == x
+      @test representative(lc) == x
+      @test Set(elements(rc)) == Set([h*x for h in H])
+      @test Set(elements(lc)) == Set([x*h for h in H])
+      @test Set(elements(dc)) == Set([h*x*k for h in H for k in K])
+   end
 
 end
 
